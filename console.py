60236fd2305f6beecad11e2074b4f9b5a45a8200
hacheG <943@holbertonschool.com>
2019-11-09 15:17:01 -0500

    [ADD] new name class

--- console.py
+++ console.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 
 import cmd
-class Commands(cmd.Cmd):
-    prompt = "(hbnb)"
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
     def do_EOF(self, args):
         """end_of_file"""
-        print("endo of file")
-    
+        print("end of file")
+        return True
+
     def do_quit(self, args):
-        """quit cmd"""
+        """Quit command to exit the program"""
         print("chao bye")
         return True
 
     def emptyline(self):
-        """no realiza nada"""
+        """don't make nothing"""
         pass
     
 if __name__ == "__main__":
-    interprete = Commands()
-    interprete.cmdloop()
+    interpret = HBNBCommand()
+    interpret.cmdloop()
