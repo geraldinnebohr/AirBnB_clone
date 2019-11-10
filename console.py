96a49e0da305f8659f2ca4901658fdc2e6577954
hacheG <943@holbertonschool.com>
2019-11-09 20:29:09 -0500

    [FEATURE] add create_function in console - Task7

--- console.py
+++ console.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+import time
+
 import cmd
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
@@ -16,7 +20,27 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """don't make nothing"""
         pass
+
+    def do_create(self, args):
+        """creates a new instance""" 
+        #print(type(args))
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        
+        token = args.split()
+
+        try:
+            newInstance = eval(token[0])()
+            #time.sleep(2)
+            newInstance.save()
+            #time.sleep(2)
+            print(newInstance.id)
+        except:
+            print("** class doesn't exist **")
+        
+            
     
 if __name__ == "__main__":
-    interpret = HBNBCommand()
-    interpret.cmdloop()
+    console = HBNBCommand()
+    console.cmdloop()
