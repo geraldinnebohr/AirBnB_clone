a8365da21f1b94530e3e4f32abad55f92ed246c6
hacheG <943@holbertonschool.com>
2019-11-10 15:19:52 -0500

    [FEATURE] add function show in console_Task7

--- console.py
+++ console.py
@@ -2,11 +2,15 @@
 
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+from models import storage
 import time
+
 
 import cmd
 class HBNBCommand(cmd.Cmd):
+
     prompt = "(hbnb) "
+
     def do_EOF(self, args):
         """end_of_file"""
         print("end of file")
@@ -38,7 +42,34 @@ class HBNBCommand(cmd.Cmd):
             print(newInstance.id)
         except:
             print("** class doesn't exist **")
+    
+    def do_show(self, args):
+        """Prints the string representation of an instance"""
+        token = args.split()
+
+        if len(args) == 0:
+            print("** class name missing **")
+            return
         
+        if token[1] == 0:
+            print("** instance id missing **")
+        
+        #print(token[0])
+        #print(token[1])
+
+        try:
+            eval(token[0])
+        except:
+            print("** class doesn't exist **")
+
+        objDict = storage.all()
+        keyId = token[0] + "." + token[1]
+
+        try:
+            value = objDict[keyId]
+            print(value)
+        except:   
+            print("** no instance found **")    
             
     
 if __name__ == "__main__":
