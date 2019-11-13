e673f1d17fd698de2e29d051d17fe61dfd8fa539
hacheG <943@holbertonschool.com>
2019-11-13 21:31:36 +0000

    [ADD] include more unittest

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -4,6 +4,7 @@
 
 import unittest
 from models.user import User
+from models.base_model import BaseModel
 from datetime import datetime
 from uuid import UUID
 from models import storage
@@ -11,6 +12,9 @@ from models import storage
 
 class TestsUser(unittest.TestCase):
     """class test user"""
+
+    obj = User()
+
     def setUp(self):
         """set initial"""
         email = ""
@@ -31,6 +35,17 @@ class TestsUser(unittest.TestCase):
         self.assertEqual(isinstance(my_object.created_at, datetime), True)
         self.assertEqual(isinstance(my_object.updated_at, datetime), True)
         self.assertEqual(type(my_object.__dict__), dict)
+
+    def test_subclass(self):
+        """test if class is subclass"""
+        self.assertEqual(issubclass(User, BaseModel), True)
+
+    def test_type(self):
+        """test type of object"""
+        self.assertEqual(type(self.obj.email), str)
+        self.assertEqual(type(self.obj.password), str)
+        self.assertEqual(type(self.obj.first_name), str)
+        self.assertEqual(type(self.obj.last_name), str)
 
 if __name__ == "__main__":
     unittest.main()
